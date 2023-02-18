c0484a44983eda42942672c295189d90692536ee
Du Duong <khaidu2002@gmail.com>
2023-02-18 17:44:56 -0600

    createstudent loop

--- src/services/students.ts
+++ src/services/students.ts
@@ -1,7 +1,7 @@
 import { addDoc, AddPrefixToKeys, collection, doc, getDocs, updateDoc } from 'firebase/firestore';
 import { db, auth } from '../firebase/clientApp'
 import { sendPermissionEmail } from '../hooks/useSendEmail';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, fetchSignInMethodsForEmail } from "firebase/auth";
 
 export interface StudentInput extends AddPrefixToKeys<string, any> {
     firstName: string;
@@ -12,23 +12,30 @@ export interface StudentInput extends AddPrefixToKeys<string, any> {
     joinPermission: boolean;
 }
 
+const findEmail = async (auth, email, name,  iter) => {
+    const newEmail = email.split('@')[0] + "+" +name+ iter + "@" + email.split('@')[1];
+    const methods = await fetchSignInMethodsForEmail(auth, newEmail);
+    // if method is empty, then the email is not in the database
+    if (methods.length != 0) {
+        return await findEmail(auth, email,name, iter + 1);
+    }
+    return newEmail;
+}
+
 export const createStudent = async (classId: string, studentInput: StudentInput) => {
     studentInput.classId = classId;
     studentInput.completedLessons = [];
     studentInput.joinPermission = false;
 
-    const password = studentInput.firstName.toLowerCase() + studentInput.lastInitial.toLowerCase() + Math.floor(Math.random() * (10));
-
+    const password = studentInput.firstName.toLowerCase() + studentInput.lastInitial.toLowerCase() + Math.floor(Math.random() * (999 - 100) + 100);
+    studentInput.parentEmail = await findEmail(auth, studentInput.parentEmail, studentInput.firstName, 1);
+    
     // this needs to be updated so that it checks if the parent email already exists in the database
     // right now it only checks twice, but in the edge case of more than 1 sibling, it may still fail.
-    createUserWithEmailAndPassword(auth, studentInput.firstName, password)
-    .catch((error) => {
-        if (error.code == "email-already-in-use")
+    createUserWithEmailAndPassword(auth, studentInput.parentEmail, password)
+    .catch(async (error) => {
         console.log(error.message);
-        studentInput.parentEmail = studentInput.parentEmail.split('@')[0] +"+"+ 
-        studentInput.firstName + "@" + studentInput.parentEmail.split('@')[1];
-        createUserWithEmailAndPassword(auth, studentInput.parentEmail, password);
-    });
+    })
 
     const studentDoc = await addDoc(collection(db, 'classes', classId, 'students'), studentInput);
 
