f1b3018cc88f6b8f68e7fd43baa7f4781f63c684
Tiger Li <tiger01tgr@gmail.com>
2023-02-18 16:25:19 -0600

    updated emailing + student account creation

--- src/services/students.ts
+++ src/services/students.ts
@@ -17,24 +17,23 @@ export const createStudent = async (classId: string, studentInput: StudentInput)
     studentInput.completedLessons = [];
     studentInput.joinPermission = false;
 
-    const password = studentInput.firstName + studentInput.lastInitial + Math.random() * (10);
-    createUserWithEmailAndPassword(auth, studentInput.firstName, password)
+    const password = studentInput.firstName + studentInput.lastInitial + Math.random() * (4);
 
     // this needs to be updated so that it checks if the parent email already exists in the database
     // right now it only checks twice, but in the edge case of more than 1 sibling, it may still fail.
+    createUserWithEmailAndPassword(auth, studentInput.firstName, password)
     .catch((error) => {
         if (error.code == "email-already-in-use")
         console.log(error.message);
         studentInput.parentEmail = studentInput.parentEmail.split('@')[0] +"+"+ 
-        studentInput.firstName + studentInput.lastInitial + "@" + studentInput.parentEmail.split('@')[1];
+        studentInput.firstName + "@" + studentInput.parentEmail.split('@')[1];
         createUserWithEmailAndPassword(auth, studentInput.parentEmail, password);
-
     });
 
     const studentDoc = await addDoc(collection(db, 'classes', classId, 'students'), studentInput);
 
     const update =  updateDoc(doc(db, 'classes', classId, 'students', studentDoc.id), { id: studentDoc.id});
-    const sendEmail = sendPermissionEmail(studentInput.firstName, studentInput.parentEmail);
+    const sendEmail = sendPermissionEmail(studentInput.firstName, studentInput.parentEmail, password);
 
     return Promise.all([update, sendEmail]);
 }
